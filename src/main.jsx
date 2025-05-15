aec7c910fac7d65f605ece24ab5fcfbcf2d2e295
Osvaldo Sanchez <osvaldopadillasanchez@gmail.com>
2025-05-15 11:42:22 -0400

    Added App.css to Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import "./assets/css/App.css"
 import App from './App.jsx'
 
 
