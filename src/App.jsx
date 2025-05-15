9953d7bba0e8389d57243720d8f5fd4d84dae911
Osvaldo Sanchez <osvaldopadillasanchez@gmail.com>
2025-05-15 10:43:29 -0400

    Add Styling to footer

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ function App() {
 
 
   return (
-    <div>
+    <div className='app-container'>
       <Header />
       <main className='app'>
         {routes}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,17 +9,19 @@ import { MdCopyright } from "react-icons/md";
 
 function Footer() {
     const date = new Date();
-    const year = date.getFullYear;
+    const year = date.getFullYear();
     return(
         <footer>
-            <div>
-                <Link to="/"><img src={logo} alt="" /></Link>
-                <div className="footer-nav">
-                    <Link to="/shop-tires"><span>Shop Tires</span></Link>
-                    <Link to="/services"><span>Services</span></Link>
-                    <Link to="/locations"><span>Locations</span></Link>
-                    <Link to="/tips-guides"><span>Tips & Guides</span></Link>
-                    <Link to="/financing"><span>Financing</span></Link>
+            <div className="top">
+                <div className="footer-links">
+                    <Link to="/"><img src={logo} alt="" /></Link>
+                    <div className="footer-nav">
+                        <Link to="/shop-tires"><span>Shop Tires</span></Link>
+                        <Link to="/services"><span>Services</span></Link>
+                        <Link to="/locations"><span>Locations</span></Link>
+                        <Link to="/tips-guides"><span>Tips & Guides</span></Link>
+                    <   Link to="/financing"><span>Financing</span></Link>
+                    </div>
                 </div>
                 <div className="hop">
                     <h1>Business Hours</h1>
@@ -28,13 +30,15 @@ function Footer() {
                     <p>Sun : Closed</p>
                 </div>
             </div>
-            <div>
+            <div className="social-media">
                 <h1>Stay In Touch</h1>
-                <FaSquareInstagram />
-                <FaFacebookSquare />
-                <AiFillTikTok />
+                <div>
+                    <a href="https://www.instagram.com/southerntiremasters/?hl=en" target="_blank"><FaSquareInstagram /></a>
+                    <a href="https://www.facebook.com/SouthernTireMasters/" target="_blank"><FaFacebookSquare /></a>
+                    <a href="https://www.tiktok.com/@southerntiremasters" target="_blank"><AiFillTikTok /></a>
+                </div>
             </div>
-            <div>
+            <div className="copyright-container">
                 <MdCopyright />
                 <p>{year}</p>
             </div>
