328605eaca961952522f4a2c5167e5297c7ad852
Osvaldo Sanchez <osvaldopadillasanchez@gmail.comm>
2025-05-17 17:10:31 -0400

    Add Finance Page hero

--- src/components/pages/Financing.jsx
+++ src/components/pages/Financing.jsx
@@ -6,7 +6,16 @@ function Financing () {
     return(
         <>
         <HelmetComponent title="Financing" description="Southern Tire Masters financing page" />
-        <img src={backgroundImage} alt="" className="financing-img"/>
+        <div className="finance-hero">
+            <img src={backgroundImage} alt="" className="financing-img"/>
+            <div className="finance-hero-content">
+                <h1>Flexible Financing with <span>Acima</span></h1>
+                <p>Get approved for $300 to $5,000 in shopping power with a quick, no-credit-impact application.</p>
+                <a href="https://apply.acima.com/?utm_source=web&utm_medium=merchant&merchant_guid=merc-96d27a68-1a54-4854-941b-a09acc476ffe" className="primary-button button acima-button">
+                    <span>Apply Now!</span>
+                </a>
+        </div>
+        </div>
         </>
     )
 }
