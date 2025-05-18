9aa60801110a4c190087ac493a98b591d6806be4
Osvaldo Sanchez <osvaldopadillasanchez@gmail.com>
2025-05-17 23:24:35 -0400

    Add blank target to Acima Link and fix font-size in Finance Page

--- src/components/pages/Financing.jsx
+++ src/components/pages/Financing.jsx
@@ -12,7 +12,7 @@ function Financing () {
             <div className="finance-hero-content">
                 <h1>Flexible Financing with <span>Acima</span></h1>
                 <p>Get approved for $300 to $5,000 in shopping power with a quick, no-credit-impact application.</p>
-                <a href="https://apply.acima.com/?utm_source=web&utm_medium=merchant&merchant_guid=merc-96d27a68-1a54-4854-941b-a09acc476ffe" className="primary-button button acima-button">
+                <a href="https://apply.acima.com/?utm_source=web&utm_medium=merchant&merchant_guid=merc-96d27a68-1a54-4854-941b-a09acc476ffe" className="primary-button button acima-button" target="_blank">
                     <span>Apply Now!</span>
                 </a>
         </div>
